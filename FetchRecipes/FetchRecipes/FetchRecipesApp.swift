09305965cb7d07d4da861d275c5fc657455b2327
Luis Puentes <lusciouslou91@yahoo.com>
2025-03-13 21:30:09 -0600

    create xcode project

---
+++ FetchRecipes/FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Luis Puentes on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipeListView()
+        }
+    }
+}
---
+++ FetchRecipes/FetchRecipes/RecipeListView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeListView.swift
+//  FetchRecipes
+//
+//  Created by Luis Puentes on 3/13/25.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeListView()
+}
