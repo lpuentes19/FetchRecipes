0d95078cfa85dc9a86d3c00c17ec5010c5bfa6a8
Luis Puentes <lusciouslou91@yahoo.com>
2025-03-13 21:36:56 -0600

    create recipe model

---
+++ FetchRecipes/FetchRecipes/Models/Recipe.swift
@@ -0,0 +1,22 @@
+//
+//  Recipe.swift
+//  FetchRecipes
+//
+//  Created by Luis Puentes on 3/13/25.
+//
+
+import Foundation
+
+struct Recipe {
+    var id: String
+    var name: String
+    var cuisine: String
+    var photoUrlSmall: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case cuisine
+        case photoUrlSmall = "photo_url_small"
+    }
+}
