8479dc93ab2bb6a9892c9b6e2303215b4b41ca1a
Luis Puentes <lusciouslou91@yahoo.com>
2025-03-14 09:49:03 -0600

    basic list view is created with fetch & sorting..
    .. function

--- FetchRecipes/FetchRecipes/Views/RecipeListView.swift
+++ FetchRecipes/FetchRecipes/Views/RecipeListView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct RecipeListView: View {
+    @State var recipes: [Recipe] = []
+    @State var isLoading: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(recipes) { recipe in
+                HStack(spacing: 16) {
+                    AsyncImage(url: URL(string: recipe.photoUrlSmall ?? ""))
+                        .frame(width: 150, height: 150)
+                        .aspectRatio(contentMode: .fit)
+                        .clipped()
+                        .cornerRadius(8)
+                    
+                    VStack(spacing: 8) {
+                        Text("Cuisine: \(recipe.cuisine)")
+                            .font(.headline)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text(recipe.name)
+                            .font(.subheadline)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
+                .listRowSeparator(.hidden)
+            }
+            .navigationTitle("Recipes")
+            .navigationBarTitleDisplayMode(.large)
+            .refreshable {
+                fetchAndSortRecipes()
+            }
+            .onAppear {
+                fetchAndSortRecipes()
+            }
+        }
+    }
+    
+    private func fetchAndSortRecipes() {
+        Task { @MainActor in
+            recipes = try await NetworkManager.shared.fetchRecipes()
+            recipes.sort {
+                $0.cuisine < $1.cuisine
+            }
+        }
     }
 }
 
