997b19d346eeab734bf5856978728d83f67c8a14
Luis Puentes <lusciouslou91@yahoo.com>
2025-03-13 22:15:35 -0600

    add network manager class and implement fetch..
    .. recipes call

---
+++ FetchRecipes/FetchRecipes/Networking/NetworkManager.swift
@@ -0,0 +1,44 @@
+//
+//  NetworkManager.swift
+//  FetchRecipes
+//
+//  Created by Luis Puentes on 3/13/25.
+//
+
+import Foundation
+
+enum NetworkingError: Error {
+    case invalidUrl
+    case invalidStatusCode
+    case requestFailed
+}
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchRecipes() async throws -> [Recipe] {
+        let urlString = "https://d3jbb8n5wk0qxi.cloudfront.net/recipes.json"
+        guard let url = URL(string: urlString) else {
+            throw NetworkingError.invalidUrl
+        }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+            
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode else {
+                throw NetworkingError.invalidStatusCode
+            }
+            
+            guard (200...299).contains(statusCode) else {
+                throw NetworkingError.invalidStatusCode
+            }
+            
+            let recipeResponse = try JSONDecoder().decode(RecipeResponse.self, from: data)
+            return recipeResponse.recipes
+        } catch {
+            throw NetworkingError.requestFailed
+        }
+    }
+}
